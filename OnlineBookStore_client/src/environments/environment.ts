e6b01ee4058706f97a2bc3e1aeaf47212570bea1
pavithra-dotnet2506 <pavithra-dotnet2506@gmail.com>
2026-08-05 10:45:45 -0500

    Configure Azure deployment

--- OnlineBookStore_client/src/environments/environment.ts
+++ OnlineBookStore_client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   //apiUrl: 'https://localhost:7016/api',
-  apiUrl: 'http://localhost:5119/api',
+  //apiUrl: 'http://localhost:5119/api',
+  apiUrl: 'https://onlinebookstore-api-2506-fbf7ehc7hza4aebn.centralus-01.azurewebsites.net/api',
 };
